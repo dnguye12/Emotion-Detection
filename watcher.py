7ba8bcc3ee7754ffaf176de2cf3a42096accf4e9
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-05-13 15:47:51 +0200

    improvement

--- watcher.py
+++ watcher.py
@@ -1,81 +1,90 @@
 import cv2
-import numpy as np
+import dlib
 from tensorflow.keras.models import model_from_json
+import numpy as np
 
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1',26950))
+BOX_COLOR = (0, 255, 0)
+TEXT_COLOR = (0, 255, 0)
+EMOTION_FRAME_THRESHOLD = 2
 
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 
                 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-emotion_last = {}
-emotion_current = {}
-
-EMOTION_THRESHOLD = 2
-frame_counter = {}
+video_stream = cv2.VideoCapture(0)
+face_detector = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
+shape_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 json_file = open('./emotion_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 emotion_model = model_from_json(loaded_model_json)
-
 emotion_model.load_weights('./emotion_model.weights.h5')
 
-cap = cv2.VideoCapture(0)
+last_predicted_time = 0
+last_predicted_emotion = ""
+
+face_emotions = {}
+trackers = {} 
 
 while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (1280, 720))
-    if not ret:
-        print(ret)
-        continue
-    face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grabbed, frame = video_stream.read()
     
-    faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
-    if len(faces) != len(emotion_last):
-        emotion_last = {}
-        emotion_current = {}
-        frame_counter = {}
-    
-    for idx, (x, y, w, h) in enumerate(faces):
-        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (0, 255, 0), 4)
-        roi_gray_frame = gray_frame[y:y+h, x:x+w]
-        cropped_img = cv2.resize(roi_gray_frame, (48, 48))
-        cropped_img = cropped_img.astype("float32") / 255.0 
-        cropped_img = np.expand_dims(np.expand_dims(cropped_img, -1), 0)
+    if grabbed:
+        frame = cv2.resize(frame, (1280, 720))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        emotion_label = emotion_dict[maxindex]
-        
-        if idx not in emotion_last:
-            emotion_last[idx] = emotion_label
-            emotion_current[idx] = emotion_label
-            frame_counter[idx] = 0
-        
-        if emotion_label != emotion_last[idx]:
-            if emotion_label == emotion_current[idx]:
-                frame_counter[idx] += 1
-            else:
-                frame_counter[idx] = 0
-                emotion_current[idx] = emotion_label
+        if not trackers:
+            trackers.clear()
+            faces = face_detector.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                if w < 30 and h<30:
+                    continue
                 
-            if frame_counter[idx] > EMOTION_THRESHOLD:
-                emotion_last[idx] = emotion_label
-                frame_counter[idx] = 0
+                tracker = dlib.correlation_tracker()
+                rect = dlib.rectangle(x, y, x+w, y+h)
+                tracker.start_track(frame, rect)
+                tracker_id = id(tracker)
+                trackers[tracker_id] = tracker
+                face_emotions[tracker_id] = {'emotions': [], 'display_emotion': ""}
+        active_trackers = {}
+        for tracker_id, tracker in trackers.items():
+            quality = tracker.update(frame)
+            pos = tracker.get_position()
+            x, y, x2, y2 = int(pos.left()), int(pos.top()), int(pos.right()), int(pos.bottom())
+            
+            if quality < 7 or x <= 0 or y <= 0 or x2 >= frame.shape[1] or y2 >= frame.shape[0] or (x2 - x) < 30 or (y2 - y) < 30:
+                continue  
+            
+            active_trackers[tracker_id] = tracker
+            
+            face_resized = cv2.resize(gray[y:y2, x:x2], (48, 48))
+            face_resized = face_resized.astype("float32") / 255.0
+            face_expanded = np.expand_dims(face_resized, axis=0)
+            face_expanded = np.expand_dims(face_expanded, axis=-1)  
+            
+            predictions = emotion_model.predict(face_expanded)
+            max_index = np.argmax(predictions[0])
+            current_emotion = list(emotion_dict.values())[max_index]
+            
+            face_emotions[tracker_id]['emotions'].append(current_emotion)
+            if len(face_emotions[tracker_id]['emotions']) > EMOTION_FRAME_THRESHOLD:
+                if all(e == face_emotions[tracker_id]['emotions'][-1] for e in face_emotions[tracker_id]['emotions'][-EMOTION_FRAME_THRESHOLD:]):
+                    face_emotions[tracker_id]['display_emotion'] = current_emotion
+                face_emotions[tracker_id]['emotions'].pop(0)
                 
-            cv2.putText(frame, emotion_last[idx], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-            byt = emotion_current[idx].encode()
-            client.send(byt)
-        else:
-            cv2.putText(frame, emotion_last[idx], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            display_emotion = face_emotions[tracker_id]['display_emotion']
+            
+            if display_emotion is not None:
+                cv2.rectangle(frame, (x, y), (x2, y2), BOX_COLOR, 2)
+                cv2.putText(frame, display_emotion, (x - 20, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, TEXT_COLOR, 2) 
+        trackers = active_trackers
         
-    cv2.imshow('Emotion Detection', frame)
+        
+         
+        cv2.imshow('Emotion Detection', frame)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+video_stream.release()
+cv2.destroyAllWindows()     
